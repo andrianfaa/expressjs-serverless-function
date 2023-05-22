c322f5b6ac9cb9b58cb2dffbcf57190c4720b858
andrianfaa <hi@anfa.my.id>
2023-05-22 07:57:13 +0700

    chore: fix build error #3

--- setup.js
+++ setup.js
@@ -1,4 +1,4 @@
-import fs from "fs";
+const fs = require("fs");
 
 const templateEnv = `
 PORT=${process.env.NODE_ENV === "dev" ? 5000 : 3000}
@@ -7,7 +7,7 @@ PORT=${process.env.NODE_ENV === "dev" ? 5000 : 3000}
 MONGODB_URL=""
 `;
 
-async function setupApp() {
+(async () => {
     const configPath = `${process.cwd()}/config`;
     const envPath = `${configPath}/${
         process.env.NODE_ENV === "dev" ? ".env.development" : ".env"
@@ -25,6 +25,4 @@ async function setupApp() {
         console.error(error);
         process.exit(1);
     }
-}
-
-setupApp();
+})();
